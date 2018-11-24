6e40d08462fcec1fd5f75a94f4c3ef03026747d0
Alex Colombo <uricaine@hotmail.com>
2018-11-24 10:08:27 +0100

    componet created

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    contador: 0
+    contador: 0,
+    posts: []
   };
 
   style = {
@@ -10,15 +11,49 @@ class Counter extends Component {
     fontWeight: "bold"
   };
 
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  renderTags() {
+    if (this.state.posts.length === 0)
+      return <li>'No hay datos que mostrar';</li>;
+    return (
+      <ul>
+        {this.state.posts.map(post => (
+          <li key={post}>{post}</li>
+        ))}
+      </ul>
+    );
+  }
+  handleIncrement = () => {
+    console.log("increment", this.state.contador);
+    this.setState({ contador: this.state.contador + 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span style={this.style} className="badge badge-success m-2">
+        <span style={this.style} className={this.getBadgeClass()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-outline-primary btn-sm">Incrementar</button>
+        <button
+          onClick={this.handleIncrement}
+          style={{ fontSize: 30 }}
+          className="btn btn-outline-primary btn-sm"
+        >
+          Incrementar
+        </button>
+        {this.renderTags()}
       </React.Fragment>
     );
+  }
+
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.contador === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
