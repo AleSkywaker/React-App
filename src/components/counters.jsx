a971679ac4c97fa1e2de08f822ae926186e6a985
Alex Colombo <uricaine@hotmail.com>
2018-11-26 22:08:01 +0100

    refactoring counter

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,13 +9,33 @@ class Counters extends Component {
       { id: 3, value: 4 },
       { id: 4, value: 0 },
       { id: 5, value: 0 }
-    ]
+    ],
+    posts: []
+  };
+
+  handleDelete = counterId => {
+    const contadores = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: contadores });
+  };
+  handleReset = () => {
+    const contadores = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: contadores });
   };
   render() {
     return (
       <div>
+        <div className="btn btn-success btn-sm m-2" onClick={this.handleReset}>
+          Reset
+        </div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} id={counter.id}>
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+          >
             <h3>Titulo {counter.id}</h3>
           </Counter>
         ))}
